0d7fb1d31a07fa254e874f5d3630377191b41260
VancheSKa <69359310+VancheSKa@users.noreply.github.com>
2022-10-16 13:02:57 +0300

    add User and HmacUtils for data encryption

--- src/com/kaivse/HmacUtils.java
+++ src/com/kaivse/HmacUtils.java
@@ -1,7 +1,30 @@
 package com.kaivse;
 
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * @author Ivan Kalinin 16.10.2022
  */
 public class HmacUtils {
+    private static final String ALGORITHM = "HmacSHA256";
+
+    public static String calculateHmac(String key, String data) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac sha256_HMAC = Mac.getInstance(ALGORITHM);
+        SecretKey secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), ALGORITHM);
+        sha256_HMAC.init(secretKey);
+
+        return byteArrayToHex(sha256_HMAC.doFinal(data.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static String byteArrayToHex(byte[] a) {
+        StringBuilder sb = new StringBuilder(a.length * 2);
+        for (byte b : a)
+            sb.append(String.format("%02x", b));
+        return sb.toString();
+    }
 }
